b64e0b47a5e13c7f6903c7fd166620920ebdd4b3
Willem Ave <willem@squareup.com>
2015-08-02 15:02:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Make all the LEDs blink at the Burn
+
+### installtion
+Make sure to get Arduino >1.6.5
+Add https://www.pjrc.com/teensy/td_download.html
+If doing trinket stuff, get trinket addons
+
+## TODO 
+
+- [x] link libraries and test led stuff
+- [ ] poc of teensy + ir sensor, @willemave in progress
+- [ ] upload schematics for led array
+- [ ] determine if https://github.com/PaulStoffregen/Audio + octows2811 works (it should)
+- [ ] add a bunch of cool visualizations (can we modulate based on FFT from Audio )
---
+++ fastled_test/fastled_test.ino
@@ -0,0 +1,28 @@
+#include <FastLED.h>
+
+//---LED SETUP STUFF
+#define LED_PIN 0
+#define COLOR_ORDER GBR
+
+#define NUM_LEDS 60       // Change to reflect the number of LEDs you have
+
+CRGB leds[NUM_LEDS];      //naming our LED array
+
+
+//------------------SETUP------------------
+void setup()  
+{
+  delay( 2000 ); // power-up safety delay
+
+  FastLED.addLeds<WS2812B, LED_PIN>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( 125 );
+ 
+}
+
+#define NUM_MODES 3
+//------------------MAIN LOOP------------------
+void loop() {
+  fill_solid(leds, NUM_LEDS, CHSV(2, 55, 55));  
+   FastLED.show(); 
+}  
+
